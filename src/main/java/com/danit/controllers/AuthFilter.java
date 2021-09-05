dc5f33dd9647dea79bb8b618d9515a11da7a5206
Slavon2020 <vereschaka.slava@gmail.com>
2021-09-05 19:29:56 +0300

    saving currentUser in session after login

--- src/main/java/com/danit/controllers/AuthFilter.java
+++ src/main/java/com/danit/controllers/AuthFilter.java
@@ -5,10 +5,9 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.http.HttpRequest;
 
 
-@WebFilter(urlPatterns = {"/*"})
+//@WebFilter(urlPatterns = {"/*"})
 public class AuthFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -16,16 +15,14 @@ public class AuthFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("do filter ....");
-        chain.doFilter(request, response);
-//        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
-//        HttpServletResponse httpServletResponse = (HttpServletResponse)response;
-//
-//        if(httpServletRequest.getSession() == null) {
-//            httpServletResponse.sendRedirect("/login");
-//        } else {
-//            chain.doFilter(request, response);
-//        }
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse)response;
+
+        if (httpServletRequest.getSession().getAttribute("currentUser") == null) {
+            httpServletResponse.sendRedirect("/login");
+        } else {
+            chain.doFilter(request, response);
+        }
     }
 
     @Override
